c308378bbd074c27b2c56ab9167d5e0529152450
wereboss <tellsrinath@gmail.com>
2018-08-13 18:24:30 +0800

    initial version

---
+++ cards.js
@@ -0,0 +1,253 @@
+// Global Vars
+var scrJSONArr = [];
+var mainlist = [];
+var mainInd;
+
+/* Display the screens corresponding to passed list of screen ID, no return value*/
+function displayScreens(listScrID, startID, scrArr) {
+  if (startID < 0) {
+    sID = 0;
+  } else {
+    sID = listScrID.indexOf(startID);
+  }
+  cntList = 0;
+  //console.log("displayScreens listScrID: " + JSON.stringify(listScrID));
+  //console.log("displayScreens sID: " + sID);
+  //console.log("displayScreens scrArr len: " + scrArr.length);
+
+  //console.log("inside while");
+
+  $(".card-screen").each(function() {
+    if (sID < listScrID.length && cntList < 3) {
+      //console.log("listScrID:" + JSON.stringify(listScrID));
+      scrObj = scrArr.find(function(value, index, array) {
+        //console.log("Checking " + value.scrID + "with list sID:" + sID);
+        return value.scrID == listScrID[sID];
+      });
+      //console.log("displayScreens scrObj: " + JSON.stringify(scrObj));
+      $(this)
+        .find("img.card-img")
+        .first()
+        .attr("src", scrObj.img);
+      $(this)
+        .children("h3.card-header")
+        .first()
+        .text("Screen " + scrObj.scrID);
+      $(this)
+        .children("div.card-body")
+        .first()
+        .text("Screen Description for Screen " + scrObj.scrID);
+      //console.log("NextScr Len:" + scrObj.nextScr.length);
+      footerObj = $(this)
+        .children("div.card-footer")
+        .first();
+      footerObj.text("");
+      footerObj.innerHTML = "";
+      newHTML = "";
+      newHTML += '<div class="dropdown">';
+      newHTML +=
+        '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Possible Next Screens</button>';
+      newHTML +=
+        '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+
+      if (scrObj.nextScr.length > 1) {
+        //console.log("inside scrObj.nextScr.length")
+        //nextArr = scrObj.nextScr
+        //priorlist = mainlist.slice(0,mainlist.indexOf(listScrID[sID])+1);
+        $.each(scrObj.nextScr, function(ini, it) {
+          //priorlist.push(it);
+          //footerObj.append('<a href="#" onclick="changeList(' + listScrID[sID] + ',' + it + ')">' + it + "</a><br>");
+          newHTML +=
+            '<a class="dropdown-item" href="#" onclick="changeList(' +
+            listScrID[sID] +
+            "," +
+            it +
+            ')">Screen ' +
+            it +
+            "</a>";
+        });
+        newHTML += " </div> </div>";
+        //console.log(newHTML);
+        footerObj.html(newHTML);
+        //footerObj.innerHTML = newHTML;
+      }
+      sID += 1;
+      cntList += 1;
+    } else {
+      //console.log("inside else");
+      if (cntList < 3) {
+        //console.log("Inside cntList>3");
+        $(this)
+          .children("img.card-img-top")
+          .first()
+          .attr("src", "temp.png");
+        $(this)
+          .children("h3.card-header")
+          .first()
+          .text("");
+        $(this)
+          .children("div.card-body")
+          .first()
+          .text("");
+        $(this)
+          .children("div.card-footer")
+          .first()
+          .text("");
+      }
+    }
+  });
+}
+
+function getNextScrn(lastScrID, scrArr) {
+  lastScr = scrArr.find(function(value, index, array) {
+    return value.scrID >= lastScrID;
+  });
+  if (lastScrID < 0) {
+    return lastScr.scrID;
+  } else {
+    return lastScr.nextScr[0];
+  }
+}
+
+/*Parse screen list to form a connected list of screens, return back screen ID list */
+function incrScreenList(scrArr) {
+  //console.log("scrnIDlist len " + scrnIDlist.length);
+  //console.log("scrArr len " + scrArr.length);
+  /*
+    if(scrnIDlist.length>0){
+        lastScrID = scrnIDlist[scrnIDlist.length - 1];
+    }
+    else {
+        lastScrID = -1;
+    }
+    if(scrnIDlist.length < 3){  
+        iniLength = 3;
+    }
+    else {
+        iniLength = scrnIDlist.length + 1;
+    }
+    */
+  if (mainlist) {
+    if (mainlist.length < 1) {
+      finalLength = 3;
+      lastScrID = -1;
+    } else {
+      if (mainInd) {
+        finalLength = mainlist.indexOf(mainInd) + 3;
+        lastScrID = mainlist[mainlist.length - 1];
+        //console.log("finalLen:" + finalLength + " lastScrID:" + lastScrID);
+      }
+    }
+  }
+  while (mainlist.length < finalLength) {
+    //console.log("Inside While " + scrnIDlist.length);
+    nScr = getNextScrn(lastScrID, scrArr);
+    if (nScr) {
+      //scrnIDlist.push(nScr);
+      mainlist.push(nScr);
+      lastScrID = mainlist[mainlist.length - 1];
+
+      if (mainlist.length == 1) {
+        mainInd = mainlist[0];
+        //console.log("length 1:" + mainInd);
+      }
+    } else {
+      break;
+    }
+  }
+  //console.log("Ready scrnIDlist " + JSON.stringify(scrnIDlist));
+  return mainlist;
+}
+
+function checkG() {
+  //console.log("Arr: " + JSON.stringify(scrJSONArr));
+  console.log("G List: " + JSON.stringify(mainlist));
+  console.log("MainInd: " + mainInd);
+}
+
+function callNext() {
+  if (mainlist.length > 2) {
+    if (mainlist.length - mainlist.indexOf(mainInd) - 1 < 3) {
+      //nScr = getNextScrn(mainlist[mainlist.indexOf(mainInd)+2],scrJSONArr);
+      //console.log("inside add");
+      nScr = getNextScrn(mainlist[mainlist.length - 1], scrJSONArr);
+      if (nScr) {
+        mainlist.push(nScr);
+        mainInd = mainlist[mainlist.indexOf(mainInd) + 1];
+        displayScreens(mainlist, mainInd, scrJSONArr);
+        //console.log("next");
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      mainInd = mainlist[mainlist.indexOf(mainInd) + 1];
+      displayScreens(mainlist, mainInd, scrJSONArr);
+      //console.log("next");
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+function callPrev() {
+  if (mainlist.indexOf(mainInd) > 0) {
+    mainInd = mainlist[mainlist.indexOf(mainInd) - 1];
+    displayScreens(mainlist, mainInd, scrJSONArr);
+    //console.log("prev");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function changeList(currInd, nextInd) {
+  //checkG();
+  mainlist = mainlist.slice(0, mainlist.indexOf(currInd) + 1);
+  //console.log("Sliced List: " + JSON.stringify(mainlist));
+  mainlist.push(nextInd);
+  //console.log("Pushed List: " + JSON.stringify(mainlist));
+  if (mainlist.indexOf(currInd) - mainlist.indexOf(mainInd) > 1) {
+    //console.log("changing mainInd");
+    mainInd = mainlist[mainlist.indexOf(mainInd) + 1];
+  }
+  //checkG();
+  if (mainInd == currInd) {
+    incrScreenList(scrJSONArr);
+  }
+  displayScreens(mainlist, mainInd, scrJSONArr);
+  checkG();
+}
+
+$(function() {
+  $(".arrow span").click(function() {
+    //console.log($(this).attr("class"));
+    //checkG();
+    //debugger;
+    if ($(this).hasClass("fa-chevron-left")) {
+      if (!callPrev()) {
+        //console.log("First");
+      }
+    } else {
+      if (!callNext()) {
+        //console.log("End");
+      }
+    }
+    checkG();
+  });
+  $.getJSON("scrn.json", function(data) {
+    //var items = [];
+    //scrJSONArr = data.screens;
+
+    $.each(data.screens, function(ini, it) {
+      scrJSONArr.push(it);
+    });
+
+    //console.log("StringiFy " + JSON.stringify(scrJSONArr));
+    //console.log("scrJSONArr len " + scrJSONArr.length);
+    displayScreens(incrScreenList(scrJSONArr), -1, scrJSONArr);
+    //incrScreenList([],scrJSONArr);
+    //checkG();
+  });
+});
